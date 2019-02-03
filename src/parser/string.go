8c741014db86652308dc2a8eebacb616abe75435
Artur A <artur.aralin97@gmail.com>
2019-02-03 21:30:17 +0300

    add: string parser

---
+++ src/parser/string.go
@@ -0,0 +1,40 @@
+package parser
+
+const (
+	StringMatchMask = "\""
+	escapeSymbol    = "\\"
+	endStringSymbol = "\""
+)
+
+func ParseSting(token *Token, cursorPosition int, input string) int {
+	currentSymbol := ""
+	l := 0
+	isHaveEscapeSymbol := false
+	acc := ""
+
+	// skip first quote
+	cursorPosition = cursorPosition + 1
+
+	for true {
+		currentSymbol = string(input[cursorPosition])
+
+		if currentSymbol == escapeSymbol {
+			isHaveEscapeSymbol = true
+		}
+
+		if currentSymbol == endStringSymbol && isHaveEscapeSymbol == false {
+			break
+		}
+
+		if currentSymbol == endStringSymbol && isHaveEscapeSymbol == true {
+			isHaveEscapeSymbol = false
+		}
+
+		acc = acc + currentSymbol
+
+		l = l + 1
+		cursorPosition = cursorPosition + 1
+	}
+
+	return cursorPosition + 1
+}
