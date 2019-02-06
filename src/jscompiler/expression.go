24575f715740025041741637bd0e17a498a49ce7
Artur A <artur.aralin97@gmail.com>
2019-02-06 22:16:11 +0300

    add: compiler

---
+++ src/jscompiler/expression.go
@@ -0,0 +1,33 @@
+package jscompiler
+
+import (
+	"strings"
+
+	"../parser"
+)
+
+const (
+	corePrefix         = "__core"
+	expressionExecuter = "E"
+)
+
+func handleExpressionName(name string) string {
+	parts := strings.Split(name, "/")
+	acc := ""
+
+	for _, part := range parts {
+		acc = acc + "[\"" + part + "\"]"
+	}
+
+	return CtxPrefix + acc
+}
+
+func HandleExpression(ex *parser.Token, args string) string {
+	acc := expressionExecuter + "(" + CtxPrefix + "." + ex.TokenValue + ", " + args + ")"
+
+	if ex.ParentToken.TokenType == "root" {
+		acc = acc + ".call(" + CtxPrefix + ")"
+	}
+
+	return acc
+}
--- src/jscompiler/jscompiler.go
+++ src/jscompiler/jscompiler.go
@@ -6,18 +6,55 @@ import (
 	"../parser"
 )
 
+const (
+	CtxPrefix  = "F"
+	isTestMode = true
+)
+
+func walkTree(node *parser.Token) string {
+	acc := ""
+	tokensLen := len(node.ChildTokens)
+	if tokensLen > 0 {
+		for idx, chT := range node.ChildTokens {
+			if chT.TokenType == "expression" {
+				acc = acc + HandleExpression(chT, walkTree(chT))
+			}
+
+			if chT.TokenType == "number" {
+				acc = acc + chT.TokenValue
+			}
+
+			if chT.TokenType == "string" {
+				acc = acc + "\"" + chT.TokenValue + "\""
+			}
+
+			if chT.TokenType == "expression" && chT.ParentToken.TokenType == "root" {
+				acc = acc + ";\n"
+			}
+
+			if chT.ParentToken.TokenType == "expression" && tokensLen-1 > idx {
+				acc = acc + ", "
+			}
+		}
+	}
+
+	return acc
+}
+
 func Compile(root *parser.Token) string {
-	// currentToken := root
-	var visited []*parser.Token
-	var t []string
-	var text = &t
+	header := "const __core = require(\"fjs-compiler/lib/core/core.js\");\n" +
+		"const " + CtxPrefix + " = {...__core};\n" +
+		"const E = __core.E;\n"
 
-	fmt.Println(text)
+	if isTestMode {
+		header = header + "exports.CTX = " + CtxPrefix + ";\n"
+	}
 
-	// ?????
-	// for visited[len(visited)-1] != root {
-	// 	fmt.Println("")
-	// }
+	header = header + "\n"
+
+	code := walkTree(root)
+
+	fmt.Println(header + code)
 
 	return ""
 }
